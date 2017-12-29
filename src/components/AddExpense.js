0ab5115c37d31722448839480f16ba5c03b13612
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-12-29 16:41:09 +0000

    Track values and validate amount

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import ExpenseForm from './ExpenseForm'
 const AddExpense = () => {
   const title = 'Add Expense'
   return (
     <div>
       <div className="container">
         <p>{title}</p>
+        <ExpenseForm />
       </div>
     </div>
   )
