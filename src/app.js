8a79bee48fec7359dce8e3c7f71e8d3ac8d3ed7c
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-12-29 17:34:26 +0000

    Implement react-dates

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
+import 'react-dates/initialize'
+import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 /*
