c9d9c2007bb5cbcba73bbcd3d16c2944f007728f
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-12-29 15:45:08 +0000

    Implement controlled input for sort by

--- src/app.js
+++ src/app.js
@@ -20,15 +20,16 @@ store.subscribe(() => {
   console.log(visibleExpenses)
 })
  */
-store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: 1000 }))
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 1000 }))
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 300, createdAt: 21000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1000 }))
 
-store.dispatch(setTextFilter('water'))
+// store.dispatch(setTextFilter('water'))
 
-setTimeout(() => {
+/* setTimeout(() => {
   store.dispatch(setTextFilter('bill'))
 }, 3000)
-
+ */
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(
   state.expenses,
