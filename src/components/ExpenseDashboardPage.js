370175176f1943d9aee7d4417d005e5563dcb1ac
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-12-29 12:25:29 +0000

    Add dynamic filtering input

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ExpenseList from './ExpenseList'
+import ExpenseListFilters from './expenseListFilters'
 // Internal Components
 
 const ExpenseDashboardPage = () => (
   <div>
-    <h1><ExpenseList /></h1>
+    <ExpenseListFilters />
+    <ExpenseList />
   </div>
 )
 
