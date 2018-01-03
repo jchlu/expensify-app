e757ed6ca0443775a95f0bff68e4b05cbf1f881a
Johnny Choudhury-Lucas <johnny@mayden.co.uk>
2018-01-03 08:24:37 +0000

    Add tests for expenses selector

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,4 +1,5 @@
 import selectExpenses from '../../selectors/expenses'
+import moment from 'moment'
 
 const expenses = [{
   id: 1,
@@ -11,13 +12,13 @@ const expenses = [{
   description: 'Credit Card',
   amount: 15550,
   note: '',
-  createdAt: -1000
+  createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
   id: 3,
   description: 'Rent',
   amount: 109500,
   note: '',
-  createdAt: 1000
+  createdAt: moment(0).add(4, 'days').valueOf()
 }]
 
 test('Should filter out expenses without matching text', () => {
@@ -29,4 +30,48 @@ test('Should filter out expenses without matching text', () => {
   }
   const result = selectExpenses(expenses, filters)
   expect(result).toEqual([expenses[2], expenses[1]])
+})
+
+test('Should filter by startDate', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: moment(0),
+    endDate: undefined
+  }
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([expenses[2], expenses[0]])
+})
+
+test('Should filter by end date', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: moment(0)
+  }
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([expenses[0], expenses[1]])
+})
+
+test('Should sort by date', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  }
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([expenses[2],expenses[0],expenses[1]])
+})
+
+test ('Should sort by amount', () => {
+  const filters = {
+    text: '',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined
+  }
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([expenses[2],expenses[1],expenses[0]])
 })
