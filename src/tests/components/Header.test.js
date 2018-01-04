ee773ebaa6ece64b1f60ca983148b0032b4a5b3f
Johnny Choudhury-Lucas <jchlu+git@me.com>
2018-01-04 19:11:51 +0000

    Update suite to use enzyme & toJson

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import Header from '../../components/Header'
 
 test('Should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer()
-  renderer.render(<Header />)
-  expect(renderer.getRenderOutput()).toMatchSnapshot()
+  const wrapper = shallow(<Header />)
+  /**
+   * Examples:
+   * expect(wrapper.find('h1').length).toBe(1) // Check for only one h1 element
+   * expect(wrapper.find('h1').text()).toBe('Expensify') // Check for element text
+  */
+
+  expect(toJson(wrapper)).toMatchSnapshot()
+  // const renderer = new ReactShallowRenderer()
+  // renderer.render(<Header />)
+  // expect(renderer.getRenderOutput()).toMatchSnapshot()
 })
