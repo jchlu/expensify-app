3c563ea3d7260e031938b0c8e118b2cb813cd1c5
Johnny Choudhury-Lucas <jchlu+git@me.com>
2018-01-21 20:34:01 +0000

    Refactor with Jest lifecycle method

--- src/tests/components/AddExpense.test.js
+++ src/tests/components/AddExpense.test.js
@@ -4,27 +4,24 @@ import { AddExpense } from '../../components/AddExpense'
 import ExpenseForm from '../../components/ExpenseForm'
 import expenses from '../fixtures/expenses'
 
-test('Should render add expense page correctly', () => {
-  const onSubmitSpy = jest.fn()
-  const historySpy = { push: jest.fn() }
-  const wrapper = shallow(
+let onSubmitSpy, historySpy, wrapper
+
+// https://facebook.github.io/jest/docs/en/api.html#beforeeachfn-timeout
+beforeEach(() => {
+  onSubmitSpy = jest.fn()
+  historySpy = { push: jest.fn() }
+  wrapper = shallow(
     <AddExpense
       onSubmit={onSubmitSpy}
       history={historySpy}
     />
   )
+})
+test('Should render add expense page correctly', () => {
   expect(wrapper).toMatchSnapshot()
 })
 
 test('Should handle onSubmit', () => {
-  const onSubmitSpy = jest.fn()
-  const historySpy = { push: jest.fn() }
-  const wrapper = shallow(
-    <AddExpense
-      onSubmit={onSubmitSpy}
-      history={historySpy}
-    />
-  )
   wrapper.find(ExpenseForm).prop('onSubmit')(expenses[1])
   expect(historySpy.push).toHaveBeenLastCalledWith('/')
   expect(onSubmitSpy).toHaveBeenLastCalledWith(expenses[1])
