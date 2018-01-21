8b8bce2c9a51d01042bdf5a4e04daa21f77a563b
Johnny Choudhury-Lucas <jchlu+git@me.com>
2018-01-21 20:27:54 +0000

    Test that Spies are called correctly

--- src/tests/components/AddExpense.test.js
+++ src/tests/components/AddExpense.test.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { AddExpense } from '../../components/AddExpense'
-// import expenses from '../fixtures/expenses'
+import ExpenseForm from '../../components/ExpenseForm'
+import expenses from '../fixtures/expenses'
 
 test('Should render add expense page correctly', () => {
   const onSubmitSpy = jest.fn()
@@ -13,4 +14,18 @@ test('Should render add expense page correctly', () => {
     />
   )
   expect(wrapper).toMatchSnapshot()
+})
+
+test('Should handle onSubmit', () => {
+  const onSubmitSpy = jest.fn()
+  const historySpy = { push: jest.fn() }
+  const wrapper = shallow(
+    <AddExpense
+      onSubmit={onSubmitSpy}
+      history={historySpy}
+    />
+  )
+  wrapper.find(ExpenseForm).prop('onSubmit')(expenses[1])
+  expect(historySpy.push).toHaveBeenLastCalledWith('/')
+  expect(onSubmitSpy).toHaveBeenLastCalledWith(expenses[1])
 })
