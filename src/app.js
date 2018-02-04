27ea5f68cd2efc3f51d5b7d9e5622a8778db751c
Johnny Choudhury-Lucas <jchlu+git@me.com>
2018-02-04 09:07:05 +0000

    Update after react-dates BC break
    The latest version required the import line
    to be placed at the top of the app
    and also in the test setup.
    This updated snapshots with extra params.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import 'react-dates/initialize'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -9,7 +10,6 @@ import getVisibleExpenses from './selectors/expenses'
 import moment from 'moment'
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
-import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
