111b6f4b242eb7b5496b2b244e1a2d3bda942c5b
Johnny Choudhury-Lucas <jchlu@me.com>
2018-02-05 11:01:52 +0000

    Check for Heroku config vars

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,20 @@
 import * as firebase from 'firebase'
-import { expensifyConfig as config } from '../../settings'
+import { expensifyConfig } from '../../settings'
 
-firebase.initializeApp(config)
+if (!expensifyConfig && process.env.FIREBASE_API) {
+  const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  }
+  firebase.initializeApp(config)
+} else {
+  firebase.initializeApp(expensifyConfig)
+}
+
 const database = firebase.database()
 
 export { firebase, database as default }
